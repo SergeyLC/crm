ced6ed811ae4d794b4947e9fd565236211ad853a
Sergey Daub <Sergey.Daub@loya.care>
2025-09-05 22:21:44 +0200

    fixed prisms migrations

--- db/prisma/migrations/20250905201112_init/migration.sql
+++ db/prisma/migrations/20250905201112_init/migration.sql
@@ -1,14 +1,28 @@
 -- CreateEnum
+CREATE TYPE "public"."UserRole" AS ENUM ('ADMIN', 'EMPLOYEE');
+
+-- CreateEnum
+CREATE TYPE "public"."UserStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'BLOCKED');
+
+-- CreateEnum
 CREATE TYPE "public"."DealStage" AS ENUM ('LEAD', 'QUALIFIED', 'CONTACTED', 'DEMO_SCHEDULED', 'PROPOSAL_SENT', 'NEGOTIATION', 'WON', 'LOST');
 
 -- CreateEnum
 CREATE TYPE "public"."DealStatus" AS ENUM ('ACTIVE', 'ARCHIVED');
+
+-- CreateEnum
+CREATE TYPE "public"."AppointmentType" AS ENUM ('CALL', 'MEETING', 'LUNCH');
+
+-- CreateEnum
+CREATE TYPE "public"."GroupRole" AS ENUM ('LEADER', 'MEMBER');
 
 -- CreateTable
 CREATE TABLE "public"."User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "role" "public"."UserRole" NOT NULL DEFAULT 'EMPLOYEE',
+    "status" "public"."UserStatus" NOT NULL DEFAULT 'ACTIVE',
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -19,7 +33,7 @@ CREATE TABLE "public"."User" (
 -- CreateTable
 CREATE TABLE "public"."Contact" (
     "id" TEXT NOT NULL,
-    "clientName" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "organization" TEXT,
     "email" TEXT,
     "phone" TEXT,
@@ -32,6 +46,7 @@ CREATE TABLE "public"."Contact" (
 -- CreateTable
 CREATE TABLE "public"."Deal" (
     "id" TEXT NOT NULL,
+    "title" TEXT,
     "contactId" TEXT NOT NULL,
     "creatorId" TEXT NOT NULL,
     "assigneeId" TEXT,
@@ -41,6 +56,7 @@ CREATE TABLE "public"."Deal" (
     "stage" "public"."DealStage" NOT NULL DEFAULT 'LEAD',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "archivedAt" TIMESTAMP(3),
 
     CONSTRAINT "Deal_pkey" PRIMARY KEY ("id")
 );
@@ -61,11 +77,33 @@ CREATE TABLE "public"."Note" (
 CREATE TABLE "public"."Appointment" (
     "id" TEXT NOT NULL,
     "datetime" TIMESTAMP(3) NOT NULL,
-    "type" TEXT NOT NULL,
-    "notes" TEXT,
+    "type" "public"."AppointmentType",
+    "note" TEXT,
     "dealId" TEXT NOT NULL,
 
     CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Group" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "leaderId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."GroupMember" (
+    "id" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "joinedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "role" "public"."GroupRole" NOT NULL DEFAULT 'MEMBER',
+
+    CONSTRAINT "GroupMember_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -88,3 +126,12 @@ ALTER TABLE "public"."Note" ADD CONSTRAINT "Note_dealId_fkey" FOREIGN KEY ("deal
 
 -- AddForeignKey
 ALTER TABLE "public"."Appointment" ADD CONSTRAINT "Appointment_dealId_fkey" FOREIGN KEY ("dealId") REFERENCES "public"."Deal"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Group" ADD CONSTRAINT "Group_leaderId_fkey" FOREIGN KEY ("leaderId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."GroupMember" ADD CONSTRAINT "GroupMember_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "public"."Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."GroupMember" ADD CONSTRAINT "GroupMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
