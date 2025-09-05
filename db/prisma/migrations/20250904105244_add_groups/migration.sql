fe994beb57109e16490b75d5f3bc6d484092cd03
Sergey Daub <Sergey.Daub@loya.care>
2025-09-05 21:24:42 +0200

    fixed .gitignore: return back db/prisma/migrations

---
+++ db/prisma/migrations/20250904105244_add_groups/migration.sql
@@ -0,0 +1,33 @@
+-- CreateEnum
+CREATE TYPE "public"."GroupRole" AS ENUM ('LEADER', 'MEMBER');
+
+-- CreateTable
+CREATE TABLE "public"."Group" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "leaderId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."GroupMember" (
+    "id" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "joinedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "role" "public"."GroupRole" NOT NULL DEFAULT 'MEMBER',
+
+    CONSTRAINT "GroupMember_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Group" ADD CONSTRAINT "Group_leaderId_fkey" FOREIGN KEY ("leaderId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."GroupMember" ADD CONSTRAINT "GroupMember_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "public"."Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."GroupMember" ADD CONSTRAINT "GroupMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
