d9ab43164a62fe39df6eb7d2edbbb5f2c9dae938
Sergey Daub <Sergey.Daub@loya.care>
2025-11-10 19:09:32 +0100

    Provide Release Tag 0.1.15 - do not clean Next.js casch

--- frontend/scripts/collect-locales.js
+++ frontend/scripts/collect-locales.js
@@ -103,13 +103,13 @@ function run() {
   }
 
   // Output namespace to path mapping
-  console.log('\nNamespace to path mapping:');
+  // console.log('\nNamespace to path mapping:');
   for (const ns of Object.keys(nsPathMap).sort()) {
     // console.log(`\nNamespace: "${ns}"`);
     for (const file of nsPathMap[ns]) {
       // Show relative path for better readability
       const relativePath = path.relative(root, file);
-      console.log(`  - ${relativePath}`);
+      // console.log(`  - ${relativePath}`);
     }
   }
 
